83c2881e4fd0aa74af83f56a0bfb98b7a7d8f104
V1ktorino <viktorlubchuk@gmail.com>
2014-01-28 23:07:48 +0200

    My PHP Lerning

--- DateTime.php
+++ DateTime.php
@@ -2,7 +2,7 @@
  
  echo time()."<br>";
  echo microtime()."<br>";
- echo @date("Y m d");
-$time = @mktime(22, 56, 33, 2, 2, 1985);// mktime(hour,minute,second,day,month,year)
+ echo date("Y m d");
+$time = mktime(22, 56, 33, 2, 2, 1985);// mktime(hour,minute,second,day,month,year)
 $get = getdate();
 var_dump($get);
