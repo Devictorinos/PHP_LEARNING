0abc138de90d317af03042f2fe7bc0edd9a45754
V1ktorino <viktorlubchuk@gmail.com>
2014-09-02 22:22:22 +0300

    setting projects

--- someTests.php
+++ someTests.php
@@ -1,73 +1,26 @@
 <?php
-$a = 5;
+
+$a = 9;
 $b = $a; // присваиваем переменой b значение а
 $b = 3; // изменяем значение b
 echo 'а =', $a, ' b = ', $b;
 
-echo "<br />";
+function foo($a, $b)
+{
+    if (is_numeric($a)) {
+        die();
+    } else {
+        return "hello";
+    }
+    return $a + $b;
+}
+
+$gg = foo($a, $b);
+;
 // когда мы изменили b на а это никак не повлияло посколько мы просто скопировали в b значение а
 // тот же пример только со сылкой
 $a = 5;
 $b = &$a; // присваиваем переменой b значение а
 $b = 6; // изменяем значение b
-echo 'а =', $a, ' b = ', $b; // как видим изменяя b мы изменили и а. Посути b это и есть а поскольку там просто сылка.
-echo "fff";
-/**
- * @author Jay Gilford
- */
-
-/**
- * get_links()
- * 
- * @param string $url
- * @return array
- */
-
-   // function getLinks($link)
-    //{
-        /*** return array ***/
-     //   $ret = array();
-
-        /*** a new dom object ***/
-      //  $dom = new domDocument;
-
-        /*** get the HTML (suppress errors) ***/
-      //  @$dom->loadHTML(file_get_contents($link));
-
-        /*** remove silly white space ***/
-      //  $dom->preserveWhiteSpace = false;
-
-        /*** get the links from the HTML ***/
-        //$links = $dom->getElementsByTagName('a');
-    
-        /*** loop over the links ***/
-      //  foreach ($links as $tag)
-      //  {
-      //      $ret[$tag->getAttribute('href')] = $tag->childNodes->item(0)->nodeValue;
-      //  }
-
-      //  return $ret;
-   // }*/
-
-
-       /*** a link to search ***/
-   // $link = "http://yad2.co.il";
-
-    /*** get the links ***/
-   // $urls = getLinks($link);
-
-    /*** check for results ***/
-   // if(sizeof($urls) > 0)
-   // {
-    //    foreach($urls as $key=>$value)
-    //    {
-     //       echo $key . ' - '. $value . '<br >';
-     //   }
-   // }
-   // else
-   // {
-      //  echo "No links found at $link";
-   // }
-
-
-    
+//echo 'а =', $a, ' b = ', $b; // как видим изменяя b мы изменили и а. Посути b это и есть а поскольку там просто сылка.
+//echo "fff";
