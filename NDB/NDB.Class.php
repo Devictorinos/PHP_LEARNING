c1fe4d6105f97b58409d4f2de4a8ac3befea7109
V1ktorino <viktorlubchuk@gmail.com>
2014-09-16 23:36:34 +0300

    new DB Work CLass with pdo in progress

--- NDB/NDB.Class.php
+++ NDB/NDB.Class.php
@@ -2,73 +2,303 @@
 
 class NDB
 {
-    private static $_dbh;
-    private static $_user;
-    private static $_pass;
-    private static $_options;
-    private static $_host;
+
+    /* COnnection Config */
+    private static $_conR = null;
+    private static $_cnnW = null;
+
+    /* DB config*/
+    private $_dbh;
+    private $_user;
+    private $_dbname;
+    private $_pass;
+    private $_options;
+    private $_host;
 
 
-    public static function setConnectionOptios($con)
+
+    /* SQL config */
+    private $_sql;
+    private $_fields;
+    private $_type;
+    private $_query;
+    private $_debug;
+
+
+    public function __construct($dbParams)
     {
-        switch ($con) {
+        $this->_host    = $dbParams['host'];
+        $this->_dbname  = $dbParams['dbname'];
+        $this->_user    = $dbParams['user'];
+        $this->_pass    = $dbParams['pass'];
+        $this->_options = $dbParams['options'];
+
+        try {
+
+            $this->_dbh = new PDO("mysql:host=" . $this->_host . ";dbname=" . $this->_dbname, $this->_user, $this->_pass, $this->_options);
+
+        } catch (PDOException $e) {
+            
+        }
+       
+    }
+
+    public static function __callStatic($method, $params)
+    {
+        if (!empty($params)) {
+            throw new Exception("Error Processing Request");
+            exit();
+        }
+
+        switch ($method) {
+
             case 'R':
-                self::$_host = "localhost";
-                self::$_user = "root";
-                self::$_pass = "123";
-                self::$_options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
-               
+
+                if (self::$_conR === null) {
+
+                    $dbParams = array ( 'host'    => "localhost",
+                                        'user'    => "root",
+                                        'pass'    => "123",
+                                        'dbname'  => "northwind",
+                                        'options' => array(
+                                                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                                                    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES hebrew",
+                                                    PDO::ATTR_TIMEOUT => 1000,
+                                                )
+                    );
+                    self::$_conR =  new self($dbParams);
+                }
+
+                return self::$_conR;
+
                 break;
+
             case 'W':
-                self::$_host = "localhost";
-                self::$_user = "root";
-                self::$_pass = "123";
-                self::$_options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
-               
+
+                if (self::$_conR === null) {
+
+                    $dbParams = array ( 'host'    => "localhost",
+                                        'user'    => "root",
+                                        'pass'    => "123",
+                                        'dbname'  => "northwind",
+                                        'options' => array(
+                                                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                                                    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES hebrew",
+                                                    PDO::ATTR_TIMEOUT => 1000,
+                                                )
+                    );
+                    self::$_conR =  new self($dbParams);
+                }
+
+                return self::$_conR;
+
                 break;
-            default:
-                # code...
-                break;
+
+
+        }
+
+    }
+
+    public function select($sql, $debug = false)
+    {
+
+        $this->_sql   = $sql;
+        $this->_debug = $debug;
+
+        preg_match_all('/[0-9]{1,}|\'.+?\'|true|false/i', $this->_sql, $matches);
+
+        $this->_fields = $matches[0];
+
+        $this->_sql    = preg_replace_callback('/[0-9]{1,}|\'.+?\'|true|false/i', function () {
+            
+            return "?";
+
+        }, $this->_sql);
+
+        if ($this->_debug === true) {
+
+            Debug::log($this->_sql, $this->_fields);
+
+        } else {
+
+            try {
+
+                $this->_query = $this->_dbh->prepare($this->_sql);
+                
+
+                foreach ($this->_fields as $key => &$field) {
+                 
+                    $this->_type = is_null($field)    ? PDO::PARAM_NULL : PDO::PARAM_STR;
+                    $this->_type = is_bool($field)    ? PDO::PARAM_BOOL : PDO::PARAM_STR;
+                    $this->_type = is_integer($field) ? PDO::PARAM_INT  : PDO::PARAM_STR;
+
+                    $this->_query->bindParam($key+1, $field, $this->_type);
+
+                }
+                
+                $this->_query->execute();
+
+                return $this;
+                
+            } catch (Exception $e) {
+
+                echo $e->getMessage();
+                
+            }
+
+        }
+       
+    }
+
+
+    /***************************************************/
+    /**************************************************/
+    /**************** FETCH METHODS ******************/
+
+
+    /* ********** FETCH FOR WHILE LOOP *********** */
+    /*=============================================*/
+    public function Fetch()
+    { 
+        if ($this->_debug === false) {
+    
+            $this->_query->setFetchMode(PDO::FETCH_ASSOC);
+            return  $this->_query->fetch();
+
         }
     }
 
-
-
-
-    public static function R()
+    public function OFetch()
     {
-        self::setConnectionOptios("R");
-        self::$_dbh = new PDO("mysql:host=" . self::$_host . ";dbname=northwind", self::$_user, self::$_pass, self::$_options);
+        if ($this->_debug === false) {
 
-        return new self;
+            $this->_query->setFetchMode(PDO::FETCH_OBJ);
+            return  $this->_query->fetch();
+
+        }
+         
+    }
+
+    public function FetchClass($className)
+    {
+        if ($this->_debug === false) {
+
+            $this->_query->setFetchMode(PDO::FETCH_CLASS, $className);
+            return  $this->_query->fetch();
+
+        }
+         
+    }
+
+    public function FetchClassAfterConstr($className)
+    {
+        if ($this->_debug === false) {
+
+            $this->_query->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, $className);
+            return  $this->_query->fetch();
+
+        }
+    }
+
+    public function FetchIntoClass($newClassCall)
+    {
+        if ($this->_debug === false) {
+
+            $this->_query->setFetchMode(PDO::FETCH_INTO, $newClassCall);
+            return  $this->_query->fetch();
+
+        }
+
     }
 
 
-
-    public static function W()
+    /* ******* FETCH ALL WITHOUT WHILE LOOP ******* */
+    /*==============================================*/
+    public function FetchAll()
     {
-        self::setConnectionOptios("W");
-        self::$_dbh = new PDO("mysql:host=" . self::$_host . ";dbname=northwind", self::$_user, self::$_pass, self::$_options);
+        if ($this->_debug === false) {
 
-        return new self;
+            $this->_query->setFetchMode(PDO::FETCH_ASSOC);
+            return  $this->_query->fetchAll();
+
+        }
+         
     }
 
 
-
-    public function select($sql)
+    public function OFetchAll()
     {
-        return new Select(self::$_dbh,  $sql);
+        if ($this->_debug === false) {
+
+            $this->_query->setFetchMode(PDO::FETCH_OBJ);
+            return  $this->_query->fetchAll();
+
+        }
+      
     }
 
 
-    public function update($fields)
+    public function FetchClassAll($className)
     {
+        if ($this->_debug === false) {
 
-        return new Update(self::$_dbh, $fields);
+            $this->_query->setFetchMode(PDO::FETCH_CLASS, $className);
+            return  $this->_query->fetchAll();
+
+        }
+        
+    }
+                   
+    public function FetchClassAllAfterConstr($className)
+    {
+        if ($this->_debug === false) {
+
+            $this->_query->setFetchMode(PDO::FETCH_CLASS|PDO::FETCH_PROP_LATE, $className);
+            return  $this->_query->fetchAll();
+
+        }
+        
     }
 
-    public function insert($fields)
+    public function FetchAllIntoClass($newClassCall)
     {
-        return new Insert(self::$_dbh, $fields);
+        if ($this->_debug === false) {
+
+            $this->_query->setFetchMode(PDO::FETCH_INTO, $newClassCall);
+            return  $this->_query->fetchAll();
+        }
+        
+    }
+
+    /*****************************************************/
+    /* *************** UPDATE METHOD ***************** */
+
+    public function Update()
+    {
+
+        
+    }
+
+
+    /*****************************************************/
+    /* *************** INSERT METHOD ***************** */
+    public function Insert()
+    {
+        
+    }
+
+    /***********************************************************/
+    /* *************** LAST INSERT ID METHOD ***************** */
+    public function GetLastInsertId()
+    {
+        
+    }
+
+
+    /*****************************************************/
+    /* *************** DELETE METHOD ***************** */
+    public function Delete()
+    {
+        
     }
 }
--- NDB/index.php
+++ NDB/index.php
@@ -1,23 +1,75 @@
 <?php
 
-
 include "NDB.Class.php";
-include "Query.Class.php";
-include "Select.Class.php";
+include "Debug.Class.php";
+
+class Employee
+{
+    public $EmployeeID;
+    public $LastName;
+    public $FirstName;
+    public $BirthDate;
+    public $Address;
+    public $City;
+    public $Region;
 
 
-$arr['name']    = "name";
-$arr['lastname'] = "lastname";
+    public function __construct()
+    {
 
-$con_token = 5;
-$sql = "SELECT * FROM `Employees` 
-        WHERE  EmployeeID IN (1,2,6,7) AND BirthDate > '1955-3-4%'";
+    }
 
-$result = NDB::R()->select($sql)->getOnyResult();
+    public function getEmD()
+    {
+        echo "Employee First Name " . $this->FirstName;
+        echo "<hr/>";
+        echo "Employee Last Name " . $this->LastName;
+        echo "<hr/>";
+        echo "Employee Birth Date " . $this->BirthDate;
+        echo "<hr/>";
+        echo "lives in City " . $this->City . " and Address " . $this->Address;
+        echo "<hr/>";
+        echo "Employee Region " . $this->Region;
+        echo "<br><br><br><br>";
 
-var_dump($result);
+    }
+}
 
-while($row  = $result->fetch(PDO::FETCH_OBJ)) {
 
+
+
+$a = 1;
+$b = 2;
+
+$employees = new Employee();
+
+$sql = "SELECT EmployeeID, LastName, FirstName, BirthDate, Address, City, Region FROM `Employees` 
+        WHERE  EmployeeID IN (".$a.",".$b.",6,7)";
+
+NDB::R()->select($sql);
+
+while ($row = NDB::R()->FetchIntoClass(new Employee())) {
+    echo "<pre>";
+    var_dump($row);
+    echo "</pre>";
+
+}
+
+
+NDB::R()->select($sql);
+
+
+while ($row  = NDB::R()->Fetch()) {
     var_dump($row);
 }
+
+$result = NDB::R()->FetchClassAll('Employee');
+
+foreach ($result as $key => $value) {
+    $value->getEmD();
+}
+
+
+
+
+
